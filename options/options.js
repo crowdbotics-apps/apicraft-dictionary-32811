48cb35d2b111d9a13e7ed5a7ab2c8251b052d189
Crowdbotics <team@crowdbotics.com>
2022-01-12 21:56:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "apicraft_dictionary_32811",
+  url: "https://apicraft_dictionary_32811.botics.co",
+  api: "https://apicraft_dictionary_32811.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
